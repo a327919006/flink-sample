9f189500aa0edfa5f6a641596f20743df317fb8a
chennan <nan.chen@cntepower.com>
2022-01-13 17:29:51 +0800

    WordCount

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # flink-sample
 
 #### 介绍
-flink示例
+
+flink入门示例
+
+#### Maven模块描述
+
+| 端口 | 模块名称 | 描述 |
+| --- | --- | --- |
+| -----       | sample-wc           | WordCount入门示例 |
---
+++ sample-wc/src/main/java/com/cn/flink/wc/WordCount.java
@@ -0,0 +1,41 @@
+package com.cn.flink.wc;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * @author Chen Nan
+ */
+public class WordCount {
+
+    public static void main(String[] args) throws Exception {
+        // 创建执行环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        // 从文件中读取数据
+        String filePath = "D:\\share\\flink-sample\\sample-wc\\src\\main\\resources\\hello.txt";
+        DataSet<String> dataSource = env.readTextFile(filePath);
+
+        dataSource.flatMap( // 逐行处理数据
+                new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                    @Override
+                    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                        String[] words = value.split(" ");
+
+                        for (String word : words) {
+                            out.collect(new Tuple2<>(word, 1));
+                        }
+                    }
+                })
+                .groupBy(0) // 按第一个位置的word分组
+                .sum(1) // 按第二个位置的数据求和
+                .print();
+    }
+}
