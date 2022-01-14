98b0b691a810f53b4068f1f73d13333bdc1f3a76
chennan <nan.chen@cntepower.com>
2022-01-14 14:18:28 +0800

    WordCount流式数据

---
+++ sample-wc/src/main/java/com/cn/flink/wc/StreamWordCount.java
@@ -0,0 +1,47 @@
+package com.cn.flink.wc;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.client.program.StreamContextEnvironment;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * @author Chen Nan
+ */
+public class StreamWordCount {
+    public static void main(String[] args) throws Exception {
+        // 创建流处理执行环境
+        StreamExecutionEnvironment env = StreamContextEnvironment.getExecutionEnvironment();
+
+        // 设置并行度，默认值 = 当前计算机的CPU逻辑核数（设置成1即单线程处理）
+        env.setParallelism(4);
+
+        // 从文件中读取数据
+//        String filePath = "D:\\share\\flink-sample\\sample-wc\\src\\main\\resources\\hello.txt";
+//        DataStream<String> dataSource = env.readTextFile(filePath);
+
+        // 从socket文本流读取数据
+        DataStream<String> dataSource = env.socketTextStream("192.168.1.222", 7777);
+
+        // 基于数据流进行转换计算
+        dataSource.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                String[] words = value.split(" ");
+
+                for (String word : words) {
+                    out.collect(new Tuple2<>(word, 1));
+                }
+            }
+        })
+                .keyBy(item -> item.f0)
+                .sum(1)
+                .print();
+
+        // 执行任务
+        env.execute();
+    }
+}
