3996d51938a00b4c476e4a7e8ee349e44fc1497f
chennan <327919006@qq.com>
2022-02-15 23:27:35 +0800

    入门示例

---
+++ sample-base/src/main/java/com/cn/flink/windows/Test5_EventWindow.java
@@ -0,0 +1,49 @@
+package com.cn.flink.windows;
+
+import com.cn.flink.domain.SensorData;
+import org.apache.flink.api.common.eventtime.WatermarkGenerator;
+import org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.OutputTag;
+
+import java.time.Duration;
+
+/**
+ * 时间语义
+ *
+ * @author Chen Nan
+ */
+public class Test5_EventWindow {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+
+        env.socketTextStream("127.0.0.1", 7777)
+                .map((MapFunction<String, SensorData>) value -> {
+                    String[] split = value.split(",");
+                    SensorData sensorData = new SensorData();
+                    sensorData.setId(Long.parseLong(split[0]));
+                    sensorData.setName(split[1]);
+                    sensorData.setValue(Double.parseDouble(split[2]));
+                    sensorData.setTimestamp(Long.parseLong(split[3]));
+                    return sensorData;
+                }, TypeInformation.of(SensorData.class))
+                .assignTimestampsAndWatermarks(WatermarkStrategy.forBoundedOutOfOrderness(Duration.ofSeconds(5)))
+                .keyBy((KeySelector<SensorData, Long>) SensorData::getId)
+                .window(TumblingEventTimeWindows.of(Time.seconds(10)))
+                .maxBy("value")
+                .print();
+
+
+        env.execute();
+    }
+}
