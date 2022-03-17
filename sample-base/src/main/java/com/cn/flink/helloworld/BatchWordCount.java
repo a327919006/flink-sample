cf481099a92b904240290c620b7136b56c2c7d0d
chennan <327919006@qq.com>
2022-03-17 20:48:50 +0800

    入门示例

--- sample-base/src/main/java/com/cn/flink/helloworld/BatchWordCount.java
+++ sample-base/src/main/java/com/cn/flink/helloworld/BatchWordCount.java
@@ -1,17 +1,19 @@
 package com.cn.flink.helloworld;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.util.Collector;
 
 import java.io.File;
 
 /**
+ * 批处理，结果统一输出
+ *
  * @author Chen Nan
  */
-public class WordCount {
+public class BatchWordCount {
 
     public static void main(String[] args) throws Exception {
         // 创建执行环境
@@ -25,17 +27,15 @@ public class WordCount {
         // 按照第一个位置的word分组
         // 按照第二个位置上的数据求和
         dataSource.flatMap(
-                new FlatMapFunction<String, Tuple2<String, Integer>>() {
-                    @Override
-                    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
-                        // value为一行数据
-                        String[] words = value.split(" ");
+                (FlatMapFunction<String, Tuple2<String, Integer>>) (value, out) -> {
+                    // value为一行数据
+                    String[] words = value.split(" ");
 
-                        for (String word : words) {
-                            out.collect(new Tuple2<>(word, 1));
-                        }
+                    for (String word : words) {
+                        out.collect(new Tuple2<>(word, 1));
                     }
                 })
+                .returns(Types.TUPLE(Types.STRING, Types.INT)) // 指定flatMap输出类型
                 .groupBy(0) // 按第一个位置的word分组
                 .sum(1)// 按第二个位置的数据求和
                 .print();
--- sample-base/src/main/java/com/cn/flink/helloworld/StreamWordCount.java
+++ sample-base/src/main/java/com/cn/flink/helloworld/StreamWordCount.java
@@ -1,20 +1,22 @@
 package com.cn.flink.helloworld;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.client.program.StreamContextEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.util.Collector;
 
 /**
+ * 流处理，结果每读一条，输出一次结果
+ *
  * @author Chen Nan
  */
 public class StreamWordCount {
     public static void main(String[] args) throws Exception {
         ParameterTool pt = ParameterTool
-                .fromPropertiesFile(WordCount.class.getResourceAsStream("/config.properties"))
+                .fromPropertiesFile(BatchWordCount.class.getResourceAsStream("/config.properties"))
                 .mergeWith(ParameterTool.fromArgs(args))
                 .mergeWith(ParameterTool.fromSystemProperties());
 
@@ -32,16 +34,15 @@ public class StreamWordCount {
         DataStream<String> dataSource = env.socketTextStream(host, port);
 
         // 基于数据流进行转换计算，每一步都支持设置并行度
-        dataSource.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
-            @Override
-            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
-                String[] words = value.split(" ");
+        dataSource.flatMap(
+                (FlatMapFunction<String, Tuple2<String, Integer>>) (value, out) -> {
+                    String[] words = value.split(" ");
 
-                for (String word : words) {
-                    out.collect(new Tuple2<>(word, 1));
-                }
-            }
-        })
+                    for (String word : words) {
+                        out.collect(new Tuple2<>(word, 1));
+                    }
+                })
+                .returns(Types.TUPLE(Types.STRING, Types.INT)) // 指定flatMap输出类型
                 .keyBy(item -> item.f0)
                 .sum(1).setParallelism(1)
                 .print().setParallelism(1);
