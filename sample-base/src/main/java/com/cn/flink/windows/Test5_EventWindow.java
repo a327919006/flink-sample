e7179dcde5b913abddd66fad8b2dec635b5fac33
chennan <327919006@qq.com>
2022-03-19 22:27:49 +0800

    入门示例

--- sample-base/src/main/java/com/cn/flink/windows/Test5_EventWindow.java
+++ sample-base/src/main/java/com/cn/flink/windows/Test5_EventWindow.java
@@ -1,24 +1,19 @@
 package com.cn.flink.windows;
 
 import com.cn.flink.domain.SensorData;
-import org.apache.flink.api.common.eventtime.WatermarkGenerator;
-import org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier;
-import org.apache.flink.api.common.eventtime.WatermarkOutput;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.streaming.api.datastream.DataStreamSink;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.util.OutputTag;
 
 import java.time.Duration;
 
 /**
- * 时间语义
+ * 时间语义，事件时间
  *
  * @author Chen Nan
  */
@@ -26,6 +21,8 @@ public class Test5_EventWindow {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
+        // 生成Watermark的周期，默认200毫秒
+        env.getConfig().setAutoWatermarkInterval(200);
 
 
         env.socketTextStream("127.0.0.1", 7777)
@@ -38,25 +35,12 @@ public class Test5_EventWindow {
                     sensorData.setTimestamp(Long.parseLong(split[3]));
                     return sensorData;
                 }, TypeInformation.of(SensorData.class))
-                .assignTimestampsAndWatermarks(WatermarkStrategy.forGenerator(new WatermarkGeneratorSupplier<SensorData>() {
-                    @Override
-                    public WatermarkGenerator<SensorData> createWatermarkGenerator(Context context) {
-                        return new WatermarkGenerator<SensorData>() {
-                            @Override
-                            public void onEvent(SensorData event, long eventTimestamp, WatermarkOutput output) {
-
-                            }
-
-                            @Override
-                            public void onPeriodicEmit(WatermarkOutput output) {
-
-                            }
-                        };
-                    }
-                }))
-                .assignTimestampsAndWatermarks(WatermarkStrategy.forBoundedOutOfOrderness(Duration.ofSeconds(5)))
+                .assignTimestampsAndWatermarks(
+                        WatermarkStrategy.<SensorData>forBoundedOutOfOrderness(Duration.ofSeconds(2))
+                                .withTimestampAssigner((SerializableTimestampAssigner<SensorData>) (element, recordTimestamp) -> element.getTimestamp())
+                )
                 .keyBy((KeySelector<SensorData, Long>) SensorData::getId)
-                .window(TumblingEventTimeWindows.of(Time.seconds(10)))
+                .window(TumblingEventTimeWindows.of(Time.seconds(3)))
                 .maxBy("value")
                 .print();
 
